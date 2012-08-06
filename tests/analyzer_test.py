2ef34ffb77705c00e1c376fe0911214a542c25c6
Tautvidas <t.9b5as@gmail.com>
2012-08-06 23:47:48 +0300

    Increasing test coverage

--- tests/analyzer_test.py
+++ tests/analyzer_test.py
@@ -1,5 +1,7 @@
 import unittest
 import os
+import mock
+import time
 from latte.Analyzer import Analyzer
 
 class TestAnalyzer(unittest.TestCase):
@@ -12,6 +14,7 @@ class TestAnalyzer(unittest.TestCase):
         self.configs['autosaveTime'] = 3600
 
         self.analyzer = Analyzer(self.configs)
+        self.allowedTimeImprecisionInSeconds = 1
 
     def tearDown(self):
         pass
@@ -25,3 +28,18 @@ class TestAnalyzer(unittest.TestCase):
         self.assertEqual(self.analyzer.normalize_time(119), '1m59s')
         self.assertEqual(self.analyzer.normalize_time(3600), '1h0m0s')
         self.assertEqual(self.analyzer.normalize_time(3661), '1h1m1s')
+
+    def testCalculateSince(self):
+        """ Tests Calculate since. """
+        self.assertEqual(self.analyzer.calculate_since('0'), 0)
+        self.assertEqual(self.analyzer.calculate_since('-1'), 0)
+        self.assertAlmostEqual(self.analyzer.calculate_since('20'), time.time() - 20, delta=self.allowedTimeImprecisionInSeconds)
+        # With multiplier
+        oneDayAgo = time.time() - 60 * 60 * 24
+        self.assertAlmostEqual(self.analyzer.calculate_since('1', 'd'), oneDayAgo, delta=self.allowedTimeImprecisionInSeconds)
+        twoWeeksAgo = time.time() - 60 * 60 * 24 * 7 * 2
+        self.assertAlmostEqual(self.analyzer.calculate_since('2', 'w'), twoWeeksAgo, delta=self.allowedTimeImprecisionInSeconds)
+        threeMonthsAgo = time.time() - 60 * 60 * 24 * 30 * 3
+        self.assertAlmostEqual(self.analyzer.calculate_since('3', 'm'), threeMonthsAgo, delta=self.allowedTimeImprecisionInSeconds)
+        # Cannot convert to number
+        self.assertFalse(self.analyzer.calculate_since('crash'))
---
+++ tests/config_test.py
@@ -0,0 +1,11 @@
+import unittest
+import os
+from latte.Config import Config
+
+class TestAnalyzer(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
